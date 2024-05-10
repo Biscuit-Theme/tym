f40251ed3d0ad910017be044978098fe673c8ed7
Winter Hille <a2405392127a@gmail.com>
2024-05-10 15:14:58 -0700

    added light theme and did a small refactor

--- biscuit-dark.lua
+++ biscuit-dark.lua
@@ -1,16 +1,29 @@
+local background = "#1a1515"
+local foreground = "#ffe9c7"
+
 return {
-  color_background = "#1a1515",
-  color_foreground = "#ffe9c7",
-  color_0  = "#2d2424",
-  color_1  = "#cf2241",
-  color_2  = "#f07942",
-  color_3  = "#e39c45",
-  color_4  = "#949f6b",
-  color_5  = "#716c97",
-  color_6  = "#ae3f8a",
-  color_7  = "#9c8181",
-  color_8  = "#725a5a",
-  color_9  = "#e3556f",
+  color_background = background,
+  color_foreground = foreground,
+
+  color_window_backgrounbd = background,
+
+  color_cursor = foreground,
+  color_cursor_foregroubd = background,
+
+  color_highlight = foreground,
+  color_highlight_foreground = background,
+
+  color_0 = "#2d2424",
+  color_1 = "#cf2241",
+  color_2 = "#f07942",
+  color_3 = "#e39c45",
+  color_4 = "#949f6b",
+  color_5 = "#716c97",
+  color_6 = "#ae3f8a",
+  color_7 = "#9c8181",
+
+  color_8 = "#725a5a",
+  color_9 = "#e3556f",
   color_10 = "#f49d75",
   color_11 = "#edbf86",
   color_12 = "#b0b893",
---
+++ biscuit-light.lua
@@ -0,0 +1,33 @@
+local background = "#FFF7EB"
+local foreground = "#2D2424"
+
+return {
+  color_background = background,
+  color_foreground = foreground,
+
+  color_window_backgrounbd = background,
+
+  color_cursor = foreground,
+  color_cursor_foregroubd = background,
+
+  color_highlight = foreground,
+  color_highlight_foreground = background,
+
+  color_0 = "#483939",
+  color_1 = "#AE4750",
+  color_2 = "#C6695D",
+  color_3 = "#CD9165",
+  color_4 = "#97916F",
+  color_5 = "#7B727E",
+  color_6 = "#83546B",
+  color_7 = "#E0CFC6",
+
+  color_8 = "#9C8181",
+  color_9 = "#C8767D",
+  color_10 = "#DB9A91",
+  color_11 = "#D4A391",
+  color_12 = "#AFA49B",
+  color_13 = "#A69DA4",
+  color_14 = "#A3728B",
+  color_15 = "#FFF7EB",
+}
